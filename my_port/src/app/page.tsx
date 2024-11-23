deacff59c66572257cda0f9d8b07883f54569ee4
*romaisa <*mahamkhurram1230@gmail.com>
2024-11-23 23:45:31 +0500

    update

--- my_port/src/app/page.tsx
+++ my_port/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import Projects from "./components/Projects";
+import Projects from "@/app/components/Project";
 import Hero from "./components/Hero";
 import About from "@/app/components/About";
 import Skills from "@/app/components/Skills";
