e78330e12a0362ee9eda57eac67e5b415f2117cb
*romaisa <*mahamkhurram1230@gmail.com>
2024-11-23 23:41:20 +0500

    update

--- my_portfolio/src/app/components/Hero.tsx
+++ my_portfolio/src/app/components/Hero.tsx
@@ -2,14 +2,14 @@ import style from "@/app/CSS/Hero.module.css"
 
 export default function Hero () {
     return (
-       <div id="hero" className={style.HeroContainer}>
+       <div id='hero' className={style.HeroContainer}>
            <div className={style.HeroChild}>
               <div className={style.HeroGrandChild}></div>
                 <div className={style.MainHeading}>
                     <div>
-                        <p  data-aos="zoom-in-down">I'm</p>
-                        <p  data-aos="zoom-in-down">Romaisa</p>
-                        <p  data-aos="zoom-in-down">Khurram</p>
+                        <p  data-aos='zoom-in-down'>I'm</p>
+                        <p  data-aos='zoom-in-down'>Romaisa</p>
+                        <p  data-aos='zoom-in-down'>Khurram</p>
                     </div>
                 </div>
             </div>
--- my_portfolio/src/app/components/Skills.tsx
+++ my_portfolio/src/app/components/Skills.tsx
@@ -5,22 +5,22 @@ export default function Skills (){
         <div className={style.SkillContainer}>
             <div className={style.SkillChild}>
                 <div>
-                    <h2 className={style.SkillHeading} data-aos="zoom-in-down">Technologies I work with</h2>
-                    <p className={style.SkillDesc} data-aos="zoom-in-down">
+                    <h2 className={style.SkillHeading} data-aos='zoom-in-down'>Technologies I work with</h2>
+                    <p className={style.SkillDesc} data-aos='zoom-in-down'>
                         I have a solid foundation in web development , specilaizing  in HTML CSS & Typescript . My Experience to using framework like React and Next.Js to create dynamic and user friendly application. i'm also proficient in yechnologies to enhance my skills and contribute effectively to projects
                     </p>
                 </div>
                 <div>
                     <div className={style.Skills}>
                         <div className={style.SkillList}>
-                            <h2 data-aos="zoom-in-down">Typescript</h2>
-                            <h2 data-aos="zoom-in-down">Next.js</h2>
-                            <h2 data-aos="zoom-in-down">Reat.js</h2>
+                            <h2 data-aos='zoom-in-down'>Typescript</h2>
+                            <h2 data-aos='zoom-in-down'>Next.js</h2>
+                            <h2 data-aos='zoom-in-down'>Reat.js</h2>
                         </div>
                         <div className={style.SkillList}>
-                            <h2 data-aos="zoom-in-down">Tailwind</h2>
-                            <h2 data-aos="zoom-in-down">HTML</h2>
-                            <h2 data-aos="zoom-in-down">CSS</h2>
+                            <h2 data-aos='zoom-in-down'>Tailwind</h2>
+                            <h2 data-aos='zoom-in-down'>HTML</h2>
+                            <h2 data-aos='zoom-in-down'>CSS</h2>
                         </div>
 
                     </div>
