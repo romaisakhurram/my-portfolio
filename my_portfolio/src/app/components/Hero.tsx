720ea7d216b987a8b65e4d2e9fca8766f5f18934
romaisaKhurram <mahamkhurram1230@gmail.com>
2025-02-24 22:25:01 +0500

    update

--- my_portfolio/src/app/components/Hero.tsx
+++ my_portfolio/src/app/components/Hero.tsx
@@ -1,4 +1,5 @@
 import style from "@/app/CSS/Hero.module.css"
+import Link from "next/link";
 
 export default function Hero () {
     return (
@@ -10,6 +11,7 @@ export default function Hero () {
                         <p  data-aos="zoom-in-down">I am</p>
                         <p  data-aos="zoom-in-down">Romaisa</p>
                         <p  data-aos="zoom-in-down">Khurram</p>
+                        <button data-aos="zoom-in-down" className={style.button}> <Link href="https://resume-delta-orpin.vercel.app/">Resume </Link> </button>
                     </div>
                 </div>
             </div>
